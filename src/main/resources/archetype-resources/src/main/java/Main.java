e9e5ed8efae0245804a7f4e5897a2a2d38d4279c
jetkiwi <jetkiwi@gmail.com>
2013-07-18 08:35:01 +0900

    Fixed a package structure bug

--- src/main/resources/archetype-resources/src/main/java/Main.java
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ${groupId}.${artifactId};
+package ${package};
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
